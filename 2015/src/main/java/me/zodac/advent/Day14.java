1a75eec49d3cda1ea4703caabd898d99dd683d54
zodac <arouge110@msn.com>
2022-11-06 16:04:44 +1300

    Solution for 2015, Day 14

---
+++ 2015/src/main/java/me/zodac/advent/Day14.java
@@ -0,0 +1,110 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2021-2022 zodac.me
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package me.zodac.advent;
+
+import java.time.Duration;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+import me.zodac.advent.pojo.Reindeer;
+
+/**
+ * Solution for 2015, Day 14.
+ *
+ * @see <a href="https://adventofcode.com/2015/day/14">AoC 2015, Day 14</a>
+ */
+public final class Day14 {
+
+    private static final int STARTING_POINTS = 0;
+    private static final int POINTS_FOR_BEING_IN_THE_LEAD = 1;
+
+    private Day14() {
+
+    }
+
+    /**
+     * Given a {@link Collection} of {@link Reindeer}, it calculates the distance each one travels in the provided {@link Duration}, then returns the
+     * furthest distance travelled.
+     *
+     * @param reindeers      the travelling {@link Reindeer}
+     * @param travelDuration the {@link Duration} of travel
+     * @return the distance of the furthest-travelled {@link Reindeer}
+     */
+    public static long distanceOfFurthestTravelledReindeer(final Collection<Reindeer> reindeers, final Duration travelDuration) {
+        return reindeers
+            .stream()
+            .map(reindeer -> reindeer.calculateDistance(travelDuration))
+            .mapToLong(Long::longValue)
+            .max()
+            .orElseThrow(() -> new IllegalStateException("Could not find a max value for input: " + reindeers));
+    }
+
+    /**
+     * Given {@link Collection} of {@link Reindeer}, it calculates the distance each one travels for every second of the provided {@link Duration}. At
+     * the end of each second, the {@link Reindeer} that has travelled the furthest is given {@value #POINTS_FOR_BEING_IN_THE_LEAD} points. The value
+     * returned is the highest score out of all the {@link Reindeer} after the {@code travelDuration} is complete.
+     *
+     * @param reindeers      the travelling {@link Reindeer}
+     * @param travelDuration the {@link Duration} of travel
+     * @return the score of the highest-scoring {@link Reindeer}
+     */
+    public static long calculateTheHighestScore(final Collection<Reindeer> reindeers, final Duration travelDuration) {
+        final long secondsToTravel = travelDuration.toSeconds();
+        final Set<String> reindeerNames = reindeers.stream().map(Reindeer::name).collect(Collectors.toSet());
+
+        final Map<String, Integer> scoresByName = HashMap.newHashMap(reindeerNames.size());
+        for (final String reindeerName : reindeerNames) {
+            scoresByName.put(reindeerName, STARTING_POINTS);
+        }
+
+        for (int i = 1; i <= secondsToTravel; i++) {
+            final Collection<String> nameOfFurthestReindeers = new HashSet<>();
+            long distanceOfFurthestReindeer = Long.MIN_VALUE;
+
+            for (final Reindeer reindeer : reindeers) {
+                final long distanceAtTime = reindeer.calculateDistance(Duration.ofSeconds(i));
+
+                if (distanceAtTime > distanceOfFurthestReindeer) {
+                    distanceOfFurthestReindeer = distanceAtTime;
+                    nameOfFurthestReindeers.clear();
+                    nameOfFurthestReindeers.add(reindeer.name());
+                } else if (distanceAtTime == distanceOfFurthestReindeer) {
+                    nameOfFurthestReindeers.add(reindeer.name());
+                }
+            }
+
+            for (final String nameOfFurthestReindeer : nameOfFurthestReindeers) {
+                final int currentScore = scoresByName.get(nameOfFurthestReindeer);
+                scoresByName.put(nameOfFurthestReindeer, currentScore + POINTS_FOR_BEING_IN_THE_LEAD);
+            }
+        }
+
+        return Collections.max(scoresByName.values());
+    }
+}
---
+++ 2015/src/main/java/me/zodac/advent/pojo/Reindeer.java
@@ -0,0 +1,89 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2021-2022 zodac.me
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package me.zodac.advent.pojo;
+
+import java.time.Duration;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Simple class representing a reindeer that can fly at a known velocity (in km/s) for a given {@link Duration}, then must rest (and not fly) for
+ * another {@link Duration}.
+ *
+ * @param name           the name of the {@link Reindeer}
+ * @param flyingVelocity the velocity of the {@link Reindeer} in km/s, when not resting
+ * @param flyingDuration the {@link Duration} that the {@link Reindeer} can fly before resting
+ * @param restDuration   the {@link Duration} that the {@link Reindeer} must rest before flying again
+ */
+public record Reindeer(String name, int flyingVelocity, Duration flyingDuration, Duration restDuration) {
+
+    private static final Pattern REINDEER_PATTERN =
+        Pattern.compile("([A-Za-z]+) can fly (\\d+) km/s for (\\d+) seconds, but then must rest for (\\d+) seconds.");
+
+    /**
+     * Creates a {@link Reindeer} from a {@link CharSequence} in the format:
+     * <pre>
+     *     [name] can fly [flyingVelocity] km/s for [flyingDuration] seconds, but then must rest for [restDuration] seconds.
+     * </pre>
+     *
+     * @param input the {@link CharSequence} to parse
+     * @return the {@link Reindeer}
+     */
+    public static Reindeer parse(final CharSequence input) {
+        final Matcher matcher = REINDEER_PATTERN.matcher(input);
+
+        if (!matcher.find()) {
+            throw new IllegalStateException("Unable to find match in input string: " + input);
+        }
+
+        final String name = matcher.group(1);
+        final int flyingVelocity = Integer.parseInt(matcher.group(2));
+        final Duration flyingDuration = Duration.ofSeconds(Integer.parseInt(matcher.group(3)));
+        final Duration restDuration = Duration.ofSeconds(Integer.parseInt(matcher.group(4)));
+
+        return new Reindeer(name, flyingVelocity, flyingDuration, restDuration);
+    }
+
+    /**
+     * Calculates the distance that the {@link Reindeer} can travel in the provided {@link Duration} of time, in KM.
+     *
+     * @param travelDuration the {@link Duration} the {@link Reindeer} will travel
+     * @return the total distance covered, in KM
+     */
+    public long calculateDistance(final Duration travelDuration) {
+        final long fullCycleDuration = flyingDuration.toSeconds() + restDuration.toSeconds();
+        final long numberOfFullCycles = travelDuration.toSeconds() / fullCycleDuration;
+        final long remainingSeconds = travelDuration.toSeconds() % fullCycleDuration;
+
+        final long totalDistanceOfFullCycles = numberOfFullCycles * flyingVelocity * flyingDuration.toSeconds();
+
+        // Remaining time might be a single second, might be almost the full flyingDuration and restDuration
+        // Since we know it is only a partial cycle, it will only fly a maximum of flyingDuration
+        final long remainingFlyingTime = Math.min(remainingSeconds, flyingDuration.toSeconds());
+        final long distanceOfLastCycle = flyingVelocity * remainingFlyingTime;
+
+        return totalDistanceOfFullCycles + distanceOfLastCycle;
+    }
+}
