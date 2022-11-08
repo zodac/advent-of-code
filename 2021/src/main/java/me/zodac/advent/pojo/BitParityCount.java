b00c4c8317e5e6d50c9750c2ce36aedb3d5e4f20
zodac <arouge110@msn.com>
2022-11-08 15:21:24 +1300

    Updating license from MIT to 0-BSD

--- 2021/src/main/java/me/zodac/advent/pojo/BitParityCount.java
+++ 2021/src/main/java/me/zodac/advent/pojo/BitParityCount.java
@@ -1,30 +1,21 @@
 /*
- * MIT License
+ * Zero-Clause BSD License
  *
  * Copyright (c) 2021-2022 zodac.me
  *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted.
  *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
+ * SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR
+ * IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
 package me.zodac.advent.pojo;
-
-import java.util.List;
 
 /**
  * Simple POJO keeping track of the count of the bit parity (the '0's and '1's) of a binary {@link String}.
@@ -38,7 +29,7 @@ public record BitParityCount(int zeros, int ones) {
     private static final char ONE_BIT = '1';
 
     /**
-     * Given a {@link List} of {@link String} binary values, we calculate the {@link BitParityCount} for a specific {@code index} of all values.
+     * Given an {@link Iterable} of {@link String} binary values, we calculate the {@link BitParityCount} for a specific {@code index} of all values.
      *
      * <p>
      * For example, take the input:
