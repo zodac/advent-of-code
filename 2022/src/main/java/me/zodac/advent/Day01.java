ef06f30d78e11da180c5706496739fdd5a7d45d3
zodac <arouge110@msn.com>
2022-12-05 08:19:30 +1300

    Adding module for 2022 challenges

---
+++ 2022/src/main/java/me/zodac/advent/Day01.java
@@ -0,0 +1,38 @@
+/*
+ * BSD Zero Clause License
+ *
+ * Copyright (c) 2021-2022 zodac.me
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
+ * SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR
+ * IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+package me.zodac.advent;
+
+/**
+ * Solution for 2022, Day 1.
+ *
+ * @see <a href="https://adventofcode.com/2022/day/1">AoC 2022, Day 1</a>
+ */
+public final class Day01 {
+
+    private Day01() {
+
+    }
+
+    public static long part1() {
+        return 0L;
+    }
+
+    public static long part2() {
+        return 0L;
+    }
+}
