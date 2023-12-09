f069e444ac5c062cee28f7f17e872c57d6b5a972
zodac <arouge110@msn.com>
2023-12-10 00:24:22 +1300

    Moving Colour class

--- common-utils/src/main/java/me/zodac/advent/pojo/Colour.java
+++ common-utils/src/main/java/me/zodac/advent/pojo/Colour.java
@@ -15,7 +15,7 @@
  * IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
-package me.zodac.advent;
+package me.zodac.advent.pojo;
 
 import java.util.Arrays;
 
--- common-utils/src/test/java/me/zodac/advent/pojo/ColourTest.java
+++ common-utils/src/test/java/me/zodac/advent/pojo/ColourTest.java
@@ -15,7 +15,7 @@
  * IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
-package me.zodac.advent;
+package me.zodac.advent.pojo;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
